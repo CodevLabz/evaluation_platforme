44f0c2f77925f3c21450b1900df6cc62cc4952c1
zouinekh <zouinekhamine@gmail.com>
2025-02-25 20:23:58 +0100

    I apolog

---
+++ middleware.ts
@@ -0,0 +1,13 @@
+// middleware.js (in root directory)
+import { NextResponse, NextRequest } from 'next/server';
+
+export function middleware(request: NextRequest) {
+  const response = NextResponse.next();
+
+  // Set CORS headers
+  response.headers.set('Access-Control-Allow-Origin', '*');
+  response.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  response.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+  return response;
+}
